d5b9e84d86b9087a464fc27fede1cf0e9db7b629
Virginia <vir.wang@hotmail.com>
2025-06-19 23:41:52 -0400

    add viewDetail & valid Edit Test case

--- src/test/java/AutoYDTest.java
+++ src/test/java/AutoYDTest.java
@@ -6,6 +6,13 @@
  * Date: June 12,2025
  */
 
+import java.awt.image.DirectColorModel;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -29,14 +36,14 @@ class AutoYDTest {
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 		if (driver != null) {
-			driver.quit();
+//			driver.quit();
 		}
 	}
 
 	@Test
 	@Order(1)
 	@DisplayName("SmokeTest")
-	void test_getTitle() {
+	void smokeTest_getTitle() {
 		String title = driver.getTitle();
 		System.out.println("title: " + title);
 	}
@@ -48,34 +55,113 @@ class AutoYDTest {
 	 */
 
 	/**
-	 * Please write all of your test cases below within order
+	 * Please write all of your test cases below in order
 	 */
 
-	@Test
+//	@Test
 	@DisplayName("addNewEntry_InvalidTestCases")
 	@Order(2)
 	void addNewEntry_InvalidTestCases() {
-		WebElement web = driver.findElement(By.linkText("Add New Entry"));
-		System.out.println("addNewEntry_InvalidTestCases Link: " + driver.getCurrentUrl());
-		web.click();
-		driver.findElement(By.id("addr_first_name")).sendKeys(" ");
+		toAddNewEntry();
+		driver.findElement(By.id("addr_first_name")).sendKeys("");
 		driver.findElement(By.id("addr_addr_line_1")).sendKeys("Danna");
 		driver.findElement(By.xpath("//input[@id='submit_button']")).click();
-//		driver.findElement(By.xpath("//input[@id='submit_button']")).click();
-//		subtnElement
-
+		driver.findElement(By.tagName("input")).click();
 	}
 
-//	@Test
+	@Test
 	@DisplayName("addNewEntry_Edit")
-	@Order(2)
-	void addNewEntry_test_Edit() {
+	@Order(50)
+	void valid_edit() {
+		toListEntries();
+		driver.findElement(By.xpath("//form[@action='./editEntry.php']//input[@type='submit']")).click();
 
+		try {
+			fillForm("Fanshawe", "", "", "1001 Fanshawe College", "", "", "", null);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
 	}
 
 //	@Test
-	@Order(1)
-	void addNewEntry_test_create2() {
-//		System.out.println("Do");
+	@DisplayName("viewDetail")
+	@Order(100)
+	void clickFirstViewDetail() {
+		toListEntries();
+
+		try {
+			List<WebElement> viewForms = driver.findElements(By.xpath("//form[@action='./viewEntry.php']"));
+			System.out.println("ViewForms. size: " + viewForms.size());
+			if (!viewForms.isEmpty()) {
+				viewForms.get(0).findElement(By.xpath(".//input[@type='submit']")).click();
+				System.out.println("Clicked the first 'View Details' button.");
+			} else {
+				System.err.println("No 'View Details' forms found.");
+				return;
+			}
+		} catch (Exception e) {
+			System.err.println("Error while clicking 'View Details': " + e.getMessage());
+			return;
+		}
+
+		driver.findElement(By.linkText("Return")).click();
+		driver.findElement(By.linkText("Return to Menu")).click();
+	}
+
+	/**
+	 * This method is directing the index page to addNewEntry web page
+	 * 
+	 */
+	private void toAddNewEntry() {
+		driver.findElement(By.linkText("Add New Entry")).click();
+		System.out.println("addNewEntry Link: " + driver.getCurrentUrl());
+	}
+
+	/**
+	 * This method is directing the index page to List All Entries web page
+	 * 
+	 */
+	private void toListEntries() {
+		driver.findElement(By.linkText("List All Entries")).click();
+		System.out.println("List All Entries Link: " + driver.getCurrentUrl());
+	}
+
+	/**
+	 * Fill into the mandatory fields
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param business
+	 * @param addr1
+	 * @param email1
+	 * @param phone1
+	 * @param web1
+	 * @param fiedlMap - this is using for putting other not mandatory field element id and values 
+	 * @throws Exception
+	 */
+	private void fillForm(String firstName, String lastName, String business, String addr1, String email1,
+			String phone1, String web1, Map<String, String> fieldMap) throws Exception {
+
+		Map<String, String> map = Map.of("addr_first_name", firstName, "addr_last_name", lastName, "addr_business",
+				business, "addr_addr_line_1", addr1, "addr_email_1", email1, "addr_phone_1", phone1, "addr_web_url_1",
+				web1);
+
+		if (fieldMap != null && !fieldMap.isEmpty()) {
+			map.putAll(fieldMap);
+		}
+
+		try {
+			for (Map.Entry<String, String> entry : map.entrySet()) {
+				driver.findElement(By.id(entry.getKey())).sendKeys(entry.getValue());
+			}
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+			throw new Exception(e.getMessage());
+		}
+		System.out.println("Successfully fill into the form");
+
+		driver.findElement(By.xpath("//input[@id='submit_button']")).click();
+		driver.findElement(By.tagName("input")).click();
 	}
 }
